c9881ecc7fd70c2892f80a5a8e6611c189835aa5
HoferZhang <mrhongfeng@163.com>
2018-08-03 20:12:49 +0800

    2

--- debugtalk.py
+++ debugtalk.py
@@ -5,10 +5,23 @@
 from pymongo import MongoClient
 import os
 
-# IP = os.environ["ip"]
-# PORT = os.environ["port"]
+IP = os.environ["ip"]
+print(IP)
+PORT = os.environ["port"]
 # PROJECT_KEY = os.environ["PROJECT_KEY"]
 
-client = MongoClient('10.10.62.146', 27017)
-print(client.list_database_names())
+# client = MongoClient('10.10.62.146', 27017)
+client = MongoClient('%s'%(IP), PORT)
+
+# 连接mydb数据库,账号密码认证
+db = client.axdoc
+db.authenticate("dev", "9kC95[yadYxKy")
+
+account = db.account
+a = account.find_one({'mobile': '15811210028'})
+
+print(a)
+
+# for i in a:
+#     print(i)
 
